4252f88150b96569b0b862169553b7d7015902f8
artday <admin@artday.com.ua>
2018-02-08 16:10:08 +0200

    Form toggle labels script set 'focus' class for labels onchange radio inputs

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -11,24 +11,59 @@
     var a = document.getElementById('active-aside'); // aside
     var aAClass = '--show-bar'; // aside activated class
     var as = false; // status of aside
-    var t = document.getElementById('aside-toggler'); // aside-toggler
     var Y = window.pageYOffset; // current Y position
-    t.onclick = function(){
-        a.classList.toggle(aAClass);
-        if(!as){
-            shadow.style.width = "100%";
-            document.body.classList.add('--noscroll');
-        } else {
-            shadow.style.width = "0px";
-            document.body.classList.remove('--noscroll');
-        }
-        as = !as;
-    };
+
     window.onscroll = function() {
         if(!as){
             if(window.pageYOffset-Y>0){h.classList.add(hAClass);}
             else {h.classList.remove(hAClass);}
             Y = window.pageYOffset;
         }
+    };
+
+    var t = document.getElementById('aside-toggler'); // aside-toggler
+    // Return if no toggle button
+    if(t){
+        t.onclick = function(){
+            a.classList.toggle(aAClass);
+            if(!as){
+                shadow.style.width = "100%";
+                document.body.classList.add('--noscroll');
+            } else {
+                shadow.style.width = "0px";
+                document.body.classList.remove('--noscroll');
+            }
+            as = !as;
+        };
+    } else {return;}
+
+})();
+
+/*
+*  Forms toggle labels
+*  'focused' labels on radio inputs checked
+*/
+(function(){
+    var togglerClass = '.auth-toggler ';
+    var togglerFocusClass = 'focus';
+    var logId = 'login'; var regId = 'register';
+    var login = document.getElementById(logId);
+    var logLabel = document.querySelector(togglerClass + "label[for="+ logId +"]");
+    var register = document.getElementById(regId);
+    var regLabel = document.querySelector(togglerClass + "label[for="+ regId +"]");
+
+    if(!login||!register||!logLabel||!regLabel){
+        return;
+    }
+    //add focus to login label first by default
+    logLabel.classList.add(togglerFocusClass);
+
+    login.onchange = function(){
+        logLabel.classList.toggle(togglerFocusClass);
+        regLabel.classList.toggle(togglerFocusClass);
+    };
+    register.onchange = function(){
+        regLabel.classList.toggle(togglerFocusClass);
+        logLabel.classList.toggle(togglerFocusClass);
     };
 })();
